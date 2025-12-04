e06e77cec3639fc624811a78707e38101b99c9a4
mcapace <mike.capace@gmail.com>
2025-12-04 18:20:22 -0500

    Make CA logo smaller in header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -32,7 +32,7 @@ export function Header() {
               alt="Cigar Aficionado"
               width={200}
               height={60}
-              className="h-10 sm:h-12 md:h-14 lg:h-16 w-auto object-contain"
+              className="h-8 sm:h-9 md:h-10 lg:h-11 w-auto object-contain"
               priority
             />
           </Link>
